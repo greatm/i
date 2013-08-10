af0cda4c2dc157a5469828f678560cb0b9c348b0
greatm <manoj.great@yahoo.com>
2013-08-10 16:28:47 +0530

    commit_date_08/10_time_6_27

--- i/Controllers/HomeController.cs
+++ i/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace i.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "welcome";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "us";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "info";
 
             return View();
         }
