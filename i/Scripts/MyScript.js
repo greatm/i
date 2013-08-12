6e3e10f36c5d70973513a9bb6774aeb2ee5f196e
greatm <manoj.great@yahoo.com>
2013-08-12 14:35:31 +0530

    commit_date_08/12_time_4_35

---
+++ i/Scripts/MyScript.js
@@ -0,0 +1,13 @@
+﻿
+
+$(function () {
+    var accordionMenuIndex = parseInt(sessionStorage.getItem("accordionMenuIndex"));
+    $("#accordionMenu").accordion({
+        active: accordionMenuIndex,
+        activate: function (event, ui) {
+            var accordionMenuIndex = $("#accordionMenu").accordion("option", "active");
+            sessionStorage.setItem("accordionMenuIndex", accordionMenuIndex);
+        }
+    });
+
+});
